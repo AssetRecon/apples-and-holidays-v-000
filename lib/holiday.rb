478e377ea6ab9d5d062ff19c5862c9f5322fe1b5
Edsel Caprice <caprice@mail.usf.edu>
2018-06-22 14:33:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
       puts "#{season.to_s.capitalize}:"
-        holiday.each do |holiday, supples|
+        holiday.each do |attribute, supplies|
             puts "#{holiday.capitalize}: #{supplies}"
         end
   end
